75b1970324aa008ef1bd560a98c3bc8b2b4705ec
Adriano Assis <89157285+adrianopassis@users.noreply.github.com>
2024-04-27 08:16:55 -0300

    Add de arquivos

---
+++ Exercicio2/src/main/java/com/algaworks/JSF2/Livro.java
@@ -0,0 +1,27 @@
+package com.algaworks.JSF2;
+
+public class Livro {
+	
+	private String titulo;
+	private String autores;
+	private String assunto;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutores() {
+		return autores;
+	}
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+}
